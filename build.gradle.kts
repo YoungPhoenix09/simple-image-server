480365c129be3376d6cf2ff9d349ad37d6c03145
Jonathan Payne <jonathnp@bellsouth.net>
2022-08-27 13:59:17 -0400

    Setup flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	kotlin("jvm") version "1.7.10"
 	kotlin("plugin.spring") version "1.7.10"
 	kotlin("plugin.jpa") version "1.7.10"
+	id("org.flywaydb.flyway") version "9.2.0"
 }
 
 group = "com.paynefulapps"
@@ -23,6 +24,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+	implementation("org.flywaydb:flyway-core")
 	runtimeOnly("org.postgresql:postgresql")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -38,4 +40,15 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+flyway {
+	url = "jdbc:postgresql://localhost:5432/image_server"
+	user = "postgres"
+	password = "postgres"
+	schemas = arrayOf("main")
+//	placeholders = [
+//		'keyABC': 'valueXYZ',
+//	'otherplaceholder': 'value123'
+//	]
 }
