6d4d18e942b80abf553895a1f045cadfa54968d6
Jonathan Payne <jonathnp@bellsouth.net>
2022-09-13 14:42:28 -0400

    Update setup/config

--- setuplocal.sh
+++ setuplocal.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 docker-compose up -d
-sleep 5
+sleep 10
 docker cp ./src/main/resources/db/initDb/create_database.sql "postgres_db":/
 docker exec -e PGPASSWORD=postgres "postgres_db" psql -h "postgres_db" -d postgres -U postgres -a -f create_database.sql
+./gradlew flywayMigrate
